e78f0a9dc0266c6a484b5eaf3623f94bead66ec2
Samuel Prasetyo <sprasetyo0511@gmail.com>
2024-01-28 20:17:09 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/newslist_rakamin/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.newslist_rakamin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.newslist_rakamin.ui.theme.NewsList_RakaminTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NewsList_RakaminTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NewsList_RakaminTheme {
+        Greeting("Android")
+    }
+}
