1f7f776047fcf3e39cae441926ad13c8310068d8
Samuel Prasetyo <sprasetyo0511@gmail.com>
2024-01-28 21:07:25 +0700

    Displaying Data to the User Interface

--- app/src/main/java/com/example/newslist_rakamin/MainActivity.kt
+++ app/src/main/java/com/example/newslist_rakamin/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.newslist_rakamin
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,34 +11,94 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newslist_rakamin.ui.theme.NewsList_RakaminTheme
+import com.squareup.picasso.Picasso
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: NewsAdapter
+    private lateinit var apiService: ApiService
+
+    private var isLoading = false
+    private var isLastPage = false
+    private var currentPage = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            NewsList_RakaminTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val apiService: ApiService = ApiClient.getClient().create(ApiService::class.java)
+        val callTopHeadlines: Call<NewsResponse> = apiService.getTopHeadlines()
+        val callAllArticles: Call<NewsResponse> = apiService.getAllArticles()
+
+        callTopHeadlines.enqueue(object : Callback<NewsResponse> {
+            override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
+                Log.e("HIT_NEWS", "Failed to fetch top headlines: ${t.message}", t)
+            }
+
+            override fun onResponse(call: Call<NewsResponse>, response: Response<NewsResponse>) {
+                if (response.isSuccessful) {
+                    val topHeadlines = response.body()
+
+                    if (topHeadlines?.articles ?.isNotEmpty() == true) {
+                        val topHeadline = topHeadlines.articles[0] // Access the first article in the list
+
+                        // Load and Display Image using Picasso
+                        topHeadline.imageUrl?.let {
+                            Picasso.get().load(it).into(binding.imgHeadlines)
+                        }
+
+                        binding.titleHeadlines.text = topHeadline.title
+                        binding.authorHeadlines.text = topHeadline.source.name
+
+                        try {
+                            val inputFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
+                            val outputFormat = DateTimeFormatter.ofPattern("dd MMM, yyyy", Locale("id"))
+
+                            val dateTime = LocalDateTime.parse(topHeadline.publishedAt, inputFormat)
+                            val formattedDate = dateTime.format(outputFormat)
+
+                            binding.publishDateHeadlines.text = formattedDate
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                            binding.publishDateHeadlines.text = topHeadline.publishedAt
+                        }
+                    } else {
+                        Log.e("HIT_NEWS", "Top headlines response is empty.")
+                    }
+                } else {
+                    Log.e("HIT_NEWS", "Top headlines response failed: ${response.code()}")
                 }
             }
-        }
-    }
-}
+        })
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        callAllArticles.enqueue(object : Callback<NewsResponse> {
+            override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
+                Log.e("HIT_NEWS", "Failed to fetch data: ${t.message}", t)
+            }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NewsList_RakaminTheme {
-        Greeting("Android")
+            override fun onResponse(call: Call<NewsResponse>, response: Response<NewsResponse>) {
+                if (response.isSuccessful) {
+                    val newsResponse = response.body()
+                    val newsModel = newsResponse?.articles ?: emptyList()
+
+                    val adapter = NewsAdapter(newsModel)
+                    binding.rvNewslist.layoutManager = LinearLayoutManager(this@MainActivity)
+                    binding.rvNewslist.adapter = adapter
+                } else {
+                    Log.e("HIT_NEWS", "Response failed: ${response.code()}")
+                }
+            }
+        })
+
     }
 }
